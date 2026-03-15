7b47debd761a5f772999f9a381d5a78eba37bc60
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:55 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Go API client for Akeyless test/staging environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = {
+      url = "github:pleme-io/substrate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, substrate, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+      mkGoLibraryCheck = (import "${substrate}/lib/go-library-check.nix").mkGoLibraryCheck;
+    in {
+      checks.default = mkGoLibraryCheck pkgs {
+        pname = "akeyless-go-test";
+        version = "0.0.0-dev";
+        src = self;
+        vendorHash = "sha256-kcw2Wy9wn4Nvgg56zRhLXY2XVZiV7FuyvQS/hQI2nIs="; # COMPUTING
+      };
+
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ go gopls gotools ];
+      };
+    });
+}
