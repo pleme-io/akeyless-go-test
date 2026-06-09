b35d058f883b902454be50f380e9135f901eff42
drzln <luis.d@akeyless.io>
2026-06-08 17:25:43 -0700

    flake: follow substrate/nixpkgs (fleet nixpkgs alignment for cache reuse)

--- flake.nix
+++ flake.nix
@@ -2,10 +2,9 @@
   description = "Go API client for Akeyless test/staging environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.follows = "substrate/nixpkgs";
     substrate = {
       url = "github:pleme-io/substrate";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
